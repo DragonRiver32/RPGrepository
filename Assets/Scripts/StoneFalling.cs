9d41c7d728ae5cc1d8aafbb381770242ceb945a2
Stepan Zadorozhnyi <stepanyshka32@gmail.com>
2019-12-28 15:37:01 +0200

    func0001#first files is saved

---
+++ Assets/Scripts/StoneFalling.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneFalling : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(this.gameObject, 0.1f);
+        }
+    }
+}
