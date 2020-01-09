a59140b725a46b2ef025bfeb5b547c246274fe00
Nicolaenko Nikita <n.shevcov906@gmail.com>
2020-01-09 18:29:08 +0200

    1. First Commit "Igor"

---
+++ Assets/Scripts/Menu_PanelSwitch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_PanelSwitch : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject PanelMenu;
+    [SerializeField]
+    private GameObject PanelLoadLevel;
+    [SerializeField]
+    private GameObject PanelOptions;
+
+    public void PanelGameIsActive()
+    {
+        PanelMenu.SetActive(true);
+        PanelLoadLevel.SetActive(false);
+        PanelOptions.SetActive(false);
+    }
+    public void PanelLoadIsActive()
+    {
+        PanelMenu.SetActive(false);
+        PanelLoadLevel.SetActive(true);
+        PanelOptions.SetActive(false);
+    }
+    public void PanelOptionsIsActive()
+    {
+        PanelMenu.SetActive(false);
+        PanelLoadLevel.SetActive(false);
+        PanelOptions.SetActive(true);
+    }
+}
